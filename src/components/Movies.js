176f4077538a0c59c4b186f0765835477d8a8f27
Andrew Waller <andrewhwaller@gmail.com>
2019-09-28 16:37:09 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,18 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Name: {novie.title}</h3>
+          <p>Time {movie.time}</p>
+          <p>Genres: </p>
+          <ul>
+            {movie.genres.map((genre, index) -> (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
