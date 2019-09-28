4c1eca569075ea5eed9a558fb422bfbcd6fa26d4
Andrew Waller <andrewhwaller@gmail.com>
2019-09-28 16:41:20 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -6,15 +6,15 @@ const Directors = () => {
     <div>
       <h1>Directors Page</h1>
       {directors.map((director, index) => (
-          <div key={index}>
-              <h3>Name: {director.name}</h3>
-              <p>Movies:</p>
-              <ul>
-                  {director.movies.map((movie, index) => (
-                      <li key={index}>{movie}</li>
-                  ))}
-              </ul>
-          </div>
+        <div key={index}>
+          <h3>Name: {director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
       ))}
     </div>
   );
