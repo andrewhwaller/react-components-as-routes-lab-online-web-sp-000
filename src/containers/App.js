83eaa176b7862cf81064d31fb56729cf784e3d2d
Andrew Waller <andrewhwaller@gmail.com>
2019-09-28 16:46:12 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ const App = (props) => {
   return (
     <Router>
       <div className="app">
-        <Navbar />
+        <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/actors" component={Actors} />
         <Route exact path="/directors" component={Directors} />\
