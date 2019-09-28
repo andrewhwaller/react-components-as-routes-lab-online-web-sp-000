7516356a4b458d69e1ab42a3a0b30a3071087dfe
Andrew Waller <andrewhwaller@gmail.com>
2019-09-28 16:38:34 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,18 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h3>Name: {director.name}</h3>
+          <p>Movies: {movie.movies}</p>
+          <p>Genres: </p>
+          <ul>
+            {movie.genres.map((genre, index) -> (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
